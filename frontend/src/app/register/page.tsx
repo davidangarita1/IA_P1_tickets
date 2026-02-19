6b8752e1c05e2bc4154e862e6d28d0ecf769eda7
David Angarita <davidangarita.sistemas@gmail.com>
2026-02-19 16:04:57 -0500

    chore: add human-check for duplicated component

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -1,5 +1,7 @@
 import CreateTicketForm from "@/components/CreateTicketForm/CreateTicketForm";
 
+// HUMAN CHECK - This component is a duplicate of the AppointmentRegistrationForm.
+
 export default function RegisterPage() {
   return <CreateTicketForm />;
 }
